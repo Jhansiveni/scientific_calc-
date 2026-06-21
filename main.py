333731ae5df340e4198c21ecb12b01b771d470af
Jhansiveni <jhansivenidesu>
2026-06-21 11:39:09 +0530

    Changed the lines of code in main.py in branchA

--- main.py
+++ main.py
@@ -17,3 +17,23 @@ def div():
     a = int(input("Enter first number: "))
     b = int(input("Enter second number: "))
     print("Division =", a / b)
+
+
+import math
+
+
+def power(base, exponent):
+    return base ** exponent
+
+def square_root(number):
+    if number < 0:
+        return "Error: Negative root"
+    return math.sqrt(number)
+
+def sine(angle_degrees):
+    return math.sin(math.radians(angle_degrees))
+
+def natural_log(number):
+    if number <= 0:
+        return "Error: Log undefined"
+    return math.log(number)
